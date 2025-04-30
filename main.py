9e075a8e672175b8fc8433b110b7fb386f42bf4a
AJ Griffiths <164574794+damwaingames@users.noreply.github.com>
2025-04-30 16:51:55 +0100

    add clock tick and delta_time tracking

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
