05ca83127d05936a10771908d6759a2e3a32a105
AJ Griffiths <164574794+damwaingames@users.noreply.github.com>
2025-05-02 07:02:31 +0100

    Initial implementation of asteroid and asteroidfield

--- circleshape.py
+++ circleshape.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x: float, y: float, radius: int) -> None:
+    def __init__(self, x: float, y: float, radius: float) -> None:
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from player import Player
 
 
@@ -15,8 +17,12 @@ def main() -> None:
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable: pygame.sprite.Group = pygame.sprite.Group()
     drawable: pygame.sprite.Group = pygame.sprite.Group()
+    asteroids: pygame.sprite.Group = pygame.sprite.Group()
     setattr(Player, "containers", (updatable, drawable))
+    setattr(Asteroid, "containers", (asteroids, updatable, drawable))
+    setattr(AsteroidField, "containers", (updatable))
     _ = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    _ = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
